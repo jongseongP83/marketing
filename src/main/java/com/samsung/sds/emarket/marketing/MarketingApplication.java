37e58a78dff42a0575c00d09930bba38c0899fa0
jongseongP83 <du2kebi@naver.com>
2023-12-13 02:13:55 +0000

    spring boot 기동 후 메시지 추가

--- src/main/java/com/samsung/sds/emarket/marketing/MarketingApplication.java
+++ src/main/java/com/samsung/sds/emarket/marketing/MarketingApplication.java
@@ -9,6 +9,7 @@ public class MarketingApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello, Spring");
 		SpringApplication.run(MarketingApplication.class, args);
+		System.out.println("Ok! Marketing Service Ready!!");
 	}
 
 }
